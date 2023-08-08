7265f234422eb6b080dc01d92678514fba6d7a07
kamczill <kam134@interia.eu>
2023-08-08 20:43:15 +0200

    Add throttle classes

--- project/project/settings.py
+++ project/project/settings.py
@@ -67,7 +67,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         # 'user.authenticate.CustomAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 from datetime import timedelta
